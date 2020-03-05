0ef9b8affa25f56293feb9c555a51aadc2a8737e
venkatesh <vsanjeevagowda@qwinix.io>
2020-03-05 09:09:47 +0530

    initial commit

---
+++ query1.log.js
@@ -0,0 +1,326 @@
+MongoDB shell version v4.2.3
+connecting to: mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("16b369ba-09bf-4144-9444-786e0cd65e4a") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+switched to db mongo-training
+{ "_id" : { "state" : "berkshire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "michigan" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "county down" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "loiret" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "cornwall" }, "totalPersons" : 2 }
+{ "_id" : { "state" : "sivas" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "uşak" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "sinop" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "marne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "northumberland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "leicestershire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "puy-de-dôme" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "maryland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardèche" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ankara" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "dordogne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "antalya" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "corrèze" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardennes" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "bas-rhin" }, "totalPersons" : 2 }
+Type "it" for more
+bye
+MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("0aba438a-0418-454b-a1be-c231d4a93246") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{ "_id" : { "state" : "berkshire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "michigan" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "county down" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "loiret" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "cornwall" }, "totalPersons" : 2 }
+{ "_id" : { "state" : "sivas" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "uşak" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "sinop" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "marne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "northumberland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "leicestershire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "puy-de-dôme" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "maryland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardèche" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ankara" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "dordogne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "antalya" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "corrèze" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardennes" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "bas-rhin" }, "totalPersons" : 2 }
+Type "it" for more
+bye
+// =========================================MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("91a14af7-db04-49fb-94d1-3a6c4fae7fc2") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{ "_id" : { "state" : "berkshire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "michigan" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "county down" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "loiret" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "cornwall" }, "totalPersons" : 2 }
+{ "_id" : { "state" : "sivas" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "uşak" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "sinop" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "marne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "northumberland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "leicestershire" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "puy-de-dôme" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "maryland" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardèche" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ankara" }, "totalPersons" : 3 }
+{ "_id" : { "state" : "dordogne" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "antalya" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "corrèze" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "ardennes" }, "totalPersons" : 1 }
+{ "_id" : { "state" : "bas-rhin" }, "totalPersons" : 2 }
+Type "it" for more
+bye
+MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("7e35d52d-859d-4670-97e6-361899f9b3b2") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+2020-03-05T08:48:35.465+0530 E  QUERY    [js] uncaught exception: TypeError: db.users.findOne(...).pretty is not a function :
+@(shell):1:1
+bye
+// =======================MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("4a9e0111-e7d8-4adc-b0ac-8aebaa1180b8") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{
+	"_id" : ObjectId("5e605b77c9e381f2a008670b"),
+	"gender" : "male",
+	"name" : {
+		"title" : "mr",
+		"first" : "carl",
+		"last" : "jacobs"
+	},
+	"location" : {
+		"street" : "6948 springfield road",
+		"city" : "arklow",
+		"state" : "wicklow",
+		"postcode" : 71309,
+		"coordinates" : {
+			"latitude" : "-29.6721",
+			"longitude" : "-154.6037"
+		},
+		"timezone" : {
+			"offset" : "-11:00",
+			"description" : "Midway Island, Samoa"
+		}
+	},
+	"email" : "carl.jacobs@example.com",
+	"login" : {
+		"uuid" : "4f591981-b214-4430-9902-70bc0faa7e81",
+		"username" : "organicladybug144",
+		"password" : "hank",
+		"salt" : "PC6Ig6sD",
+		"md5" : "d94aac977512cb2bb005dfa360b40018",
+		"sha1" : "a5ffeb65557693e443e195bdf9c066dca33dc47d",
+		"sha256" : "f9aa851b943d9a8a876062e48b91b9af190a37779df009a20bc268c25ce48a7f"
+	},
+	"dob" : {
+		"date" : "1984-09-30T01:20:26Z",
+		"age" : 33
+	},
+	"registered" : {
+		"date" : "2008-10-29T02:25:24Z",
+		"age" : 9
+	},
+	"phone" : "031-501-5147",
+	"cell" : "081-090-3541",
+	"id" : {
+		"name" : "PPS",
+		"value" : "9806982T"
+	},
+	"picture" : {
+		"large" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"medium" : "https://randomuser.me/api/portraits/med/men/44.jpg",
+		"thumbnail" : "https://randomuser.me/api/portraits/thumb/men/44.jpg"
+	},
+	"nat" : "IE"
+}
+bye
+MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("82b67bcd-5148-449a-b061-ffc3d860af6b") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{
+	"_id" : ObjectId("5e605b77c9e381f2a008670b"),
+	"gender" : "male",
+	"name" : {
+		"title" : "mr",
+		"first" : "carl",
+		"last" : "jacobs"
+	},
+	"location" : {
+		"street" : "6948 springfield road",
+		"city" : "arklow",
+		"state" : "wicklow",
+		"postcode" : 71309,
+		"coordinates" : {
+			"latitude" : "-29.6721",
+			"longitude" : "-154.6037"
+		},
+		"timezone" : {
+			"offset" : "-11:00",
+			"description" : "Midway Island, Samoa"
+		}
+	},
+	"email" : "carl.jacobs@example.com",
+	"login" : {
+		"uuid" : "4f591981-b214-4430-9902-70bc0faa7e81",
+		"username" : "organicladybug144",
+		"password" : "hank",
+		"salt" : "PC6Ig6sD",
+		"md5" : "d94aac977512cb2bb005dfa360b40018",
+		"sha1" : "a5ffeb65557693e443e195bdf9c066dca33dc47d",
+		"sha256" : "f9aa851b943d9a8a876062e48b91b9af190a37779df009a20bc268c25ce48a7f"
+	},
+	"dob" : {
+		"date" : "1984-09-30T01:20:26Z",
+		"age" : 33
+	},
+	"registered" : {
+		"date" : "2008-10-29T02:25:24Z",
+		"age" : 9
+	},
+	"phone" : "031-501-5147",
+	"cell" : "081-090-3541",
+	"id" : {
+		"name" : "PPS",
+		"value" : "9806982T"
+	},
+	"picture" : {
+		"large" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"medium" : "https://randomuser.me/api/portraits/med/men/44.jpg",
+		"thumbnail" : "https://randomuser.me/api/portraits/thumb/men/44.jpg"
+	},
+	"nat" : "IE"
+}
+bye
+// ======================MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("5ca89ede-3e70-4046-98da-fc1740fdbee2") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{
+	"_id" : ObjectId("5e605b77c9e381f2a008670b"),
+	"gender" : "male",
+	"name" : {
+		"title" : "mr",
+		"first" : "carl",
+		"last" : "jacobs"
+	},
+	"location" : {
+		"street" : "6948 springfield road",
+		"city" : "arklow",
+		"state" : "wicklow",
+		"postcode" : 71309,
+		"coordinates" : {
+			"latitude" : "-29.6721",
+			"longitude" : "-154.6037"
+		},
+		"timezone" : {
+			"offset" : "-11:00",
+			"description" : "Midway Island, Samoa"
+		}
+	},
+	"email" : "carl.jacobs@example.com",
+	"login" : {
+		"uuid" : "4f591981-b214-4430-9902-70bc0faa7e81",
+		"username" : "organicladybug144",
+		"password" : "hank",
+		"salt" : "PC6Ig6sD",
+		"md5" : "d94aac977512cb2bb005dfa360b40018",
+		"sha1" : "a5ffeb65557693e443e195bdf9c066dca33dc47d",
+		"sha256" : "f9aa851b943d9a8a876062e48b91b9af190a37779df009a20bc268c25ce48a7f"
+	},
+	"dob" : {
+		"date" : "1984-09-30T01:20:26Z",
+		"age" : 33
+	},
+	"registered" : {
+		"date" : "2008-10-29T02:25:24Z",
+		"age" : 9
+	},
+	"phone" : "031-501-5147",
+	"cell" : "081-090-3541",
+	"id" : {
+		"name" : "PPS",
+		"value" : "9806982T"
+	},
+	"picture" : {
+		"large" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"medium" : "https://randomuser.me/api/portraits/med/men/44.jpg",
+		"thumbnail" : "https://randomuser.me/api/portraits/thumb/men/44.jpg"
+	},
+	"nat" : "IE"
+}
+bye
+// =======================MongoDB shell version v4.2.3
+connecting to: mongodb://localhost:27017/mongo-training?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("37414c2e-f851-4f08-ad5e-d8906af450f8") }
+MongoDB server version: 3.6.3
+WARNING: shell and server versions do not match
+{
+	"_id" : ObjectId("5e605b77c9e381f2a008670b"),
+	"gender" : "male",
+	"name" : {
+		"title" : "mr",
+		"first" : "carl",
+		"last" : "jacobs"
+	},
+	"location" : {
+		"street" : "6948 springfield road",
+		"city" : "arklow",
+		"state" : "wicklow",
+		"postcode" : 71309,
+		"coordinates" : {
+			"latitude" : "-29.6721",
+			"longitude" : "-154.6037"
+		},
+		"timezone" : {
+			"offset" : "-11:00",
+			"description" : "Midway Island, Samoa"
+		}
+	},
+	"email" : "carl.jacobs@example.com",
+	"login" : {
+		"uuid" : "4f591981-b214-4430-9902-70bc0faa7e81",
+		"username" : "organicladybug144",
+		"password" : "hank",
+		"salt" : "PC6Ig6sD",
+		"md5" : "d94aac977512cb2bb005dfa360b40018",
+		"sha1" : "a5ffeb65557693e443e195bdf9c066dca33dc47d",
+		"sha256" : "f9aa851b943d9a8a876062e48b91b9af190a37779df009a20bc268c25ce48a7f"
+	},
+	"dob" : {
+		"date" : "1984-09-30T01:20:26Z",
+		"age" : 33
+	},
+	"registered" : {
+		"date" : "2008-10-29T02:25:24Z",
+		"age" : 9
+	},
+	"phone" : "031-501-5147",
+	"cell" : "081-090-3541",
+	"id" : {
+		"name" : "PPS",
+		"value" : "9806982T"
+	},
+	"picture" : {
+		"large" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"medium" : "https://randomuser.me/api/portraits/med/men/44.jpg",
+		"thumbnail" : "https://randomuser.me/api/portraits/thumb/men/44.jpg"
+	},
+	"nat" : "IE"
+}
+bye
